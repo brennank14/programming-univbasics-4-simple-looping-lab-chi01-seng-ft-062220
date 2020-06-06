82eb7175307a28ce934030281b9fa2e38280da7d
Kathleen Brennan <brennank14@gmail.com>
2020-06-06 01:15:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,8 +17,12 @@ def loop_message_n_times(string, integer)
 end
 
 
-def output_array
-
+def output_array(array)
+  counter = 0
+  while array[counter] do
+    puts array[counter]
+    counter += 1
+  end
 end
 
 
