a845ce2f89ea22813cf4097c9ec63f265491f4ec
Kathleen Brennan <brennank14@gmail.com>
2020-06-06 01:25:06 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -28,8 +28,10 @@ end
 
 def return_string_array(array)
   counter = 0
+  array2 = []
   while counter < array.length do
-    array[counter].to_s
+    array2.push(array[counter].to_s)
     counter += 1
   end
+  array2
 end
